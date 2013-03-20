bca46c27399170e5a7832d88dd80fb8c576951af
Nick <npetrov.fedora@gmail.com>
2013-03-20 17:45:49 +0200

    init commit

---
+++ PopulateUITableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PopulateUITableView
+//
+//  Created by nick on 1/26/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopulateUITableView/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  PopulateUITableView
+//
+//  Created by nick on 1/26/13.
+//  Copyright (c) 2013 Nick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+@property (strong, nonatomic) NSArray *content;
+@end
